27ada14955f15801277fb015743019b3c1763464
ikram713 <i.messaoud@esi-sba.dz>
2025-01-18 23:44:47 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app=express();
+
+
+
+//connection to mongodb
+mongoose.connect("mongodb://localhost/todo_express", {
+})
+
+//midlleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+//routes
+app.use(require("./routes/index"));
+app.use(require("./routes/todo"));
+
+//sever configuration
+app.listen(3000, () => console.log("Server started listening on port : 3000"));
